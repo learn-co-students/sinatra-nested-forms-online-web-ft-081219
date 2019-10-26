4784a33d3227eb98b5434272be55bedda396f517
your_name <raul.a.figueroa@hotmail.com>
2019-10-26 19:42:40 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
+require 'pry' 
 class Ship
+    attr_accessor :name, :type, :booty
+    @@all =[]
+    def initialize(params)
+        binding.pry
+        @name = params[:name]
+        @type = params[:type]
+        @booty = params[:booty]        
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear 
+        @@all.clear
+    end
 end
